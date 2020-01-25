913bf695b62e00390fe09a5e10c8f68fd76d39c8
Rianna Cleary <rianna.cleary@gmail.com>
2020-01-25 16:34:59 -0500

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
 class Formatter {
-  //add static methods here
+  static capitalize(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1)
+  }
+  static sanitize(string) {
+    return string.replace(/[^A-Za-z0-9 '-]+/g, '')
+  }
+  static titleize(string) {
+    let wordsArray = string.split(" ")
+    let newArray = []
+    let exceptions = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from']
+
+    for (let i = 0; i < wordsArray.length; i++) {
+      if (i === 0) {
+        newArray.push(this.capitalize(wordsArray[i]))
+      } else if (exceptions.includes(wordsArray[i])) {
+        newArray.push(wordsArray[i])
+      } else {
+        newArray.push(this.capitalize(wordsArray[i]))
+      }
+    }
+    return newArray.join(' ')
+  }
 }
