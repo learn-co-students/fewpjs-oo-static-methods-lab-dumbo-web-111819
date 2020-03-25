0d1836d99be39d1b4e8b22997d7b19dcb3cf9620
ajshemi <akpojotor@yahoo.com>
2020-03-25 17:36:01 -0400

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
 class Formatter {
   //add static methods here
+
+    static capitalize(str) {
+      return `${str[0].toUpperCase()}${str.slice(1)}`
+		// console.log('My method is static!');
+    }
+
+    static sanitize(str) {
+    // console.log('My method is static!');
+    return str.replace(/[^A-Za-z0-9-'\s]+/g, '');
+
+    }
+    static titleize(arr) {
+      let testArr=['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from']
+      arr=arr.slice().split(' ')
+      let newArr=[this.capitalize(arr[0])]
+      for(let i=1;i<arr.length;i++){
+        testArr.indexOf(arr[i])===-1 ? newArr.push(this.capitalize(arr[i])) : newArr.push(arr[i])
+      }
+      // console.log(newArr.join(' ')) 
+      return newArr.join(' ')
+    }
 }
